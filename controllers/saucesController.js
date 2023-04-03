a32b43f39b459150c7a6e39a114162811967ef13
BastianGH <bastianpussey16@gmail.com>
2023-04-03 18:38:28 +0200

    corrections controlleur

--- controllers/saucesController.js
+++ controllers/saucesController.js
@@ -1,27 +1,26 @@
 const Sauce = require('../models/saucesModel');
 
 exports.getSauces = (req,res, next) => {
-  console.log("bonjour")
+  console.log("affichage de toutes les sauces")
     Sauce.find()
     .then((sauce) => { 
       res.status(200).json(sauce)
-      console.log(sauce)
      })
     .catch((error) => { 
       res.status(400).json({error})
-      console.log(error) 
      });
 };
 
 exports.getSauce = (req, res, next) => {
-    Sauce.findOne({ id: req.params.id })
+  console.log("affichage de la sauce")
+    Sauce.findOne({ _id: req.params.id })
       .then(sauce => res.status(200).json(sauce))
       .catch(error => res.status(404).json({ error }));
 }
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
-    console.log("route sauce")
+    console.log("route pour créer la sauce")
     console.log(sauceObject)
     delete sauceObject.id;
     delete sauceObject.userId;
@@ -39,13 +38,31 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, id: req.params.id })
+  console.log("modification de la sauce")
+  Sauce.findOne({ _id: req.params.id })
+  .then((sauce) => {
+    sauce.userId 
+    if(sauce.userId===req.auth.userId) {
+      Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
+    }else {
+      res.status(403).json({message: 'Unauthorized request'})
+    }
+    })    
 };
 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.deleteOne({ id: req.params.id })
+  console.log("suppression de la sauce")
+  Sauce.findOne({ _id: req.params.id })
+  .then((sauce) => {
+    sauce.userId 
+    if(sauce.userId===req.auth.userId) {
+    Sauce.deleteOne({ _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
       .catch(error => res.status(400).json({ error }));
+    }else {
+      res.status(403).json({message: 'Unauthorized request'})
+    }
+  })    
 }; 
